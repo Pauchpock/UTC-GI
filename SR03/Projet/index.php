868e4614186fc44ed0b736ac3b5f6d9931564c43
Romain Pellerin <contact@romainpellerin.eu>
2016-03-18 15:08:03 +0100

    Improve SR03 project

--- SR03/Projet/index.php
+++ SR03/Projet/index.php
@@ -12,8 +12,25 @@
 
         <?php
 
-          if (isset($_GET) && isset($_GET['login']) && !empty($_GET['login'])) { ?>
-          <!--<img src="loader.gif" alt="Loading..." /><br />-->
+        $colors = array('blue','yellow','green','red','purple','#7CFC00','magenta','gray','gray');
+        $uvsColor = array();
+
+        function getOffset($uv) {
+            $start = explode(":", $uv['begin']);
+            $end = explode(":", $uv['end']);
+            $diff_hours = ((int)$end[0]) - ((int)$start[0]);
+            $diff_minutes = ((int)$end[1]) - ((int)$start[1]);
+            return ($diff_hours * 4) + ($diff_minutes / 15);
+        }
+
+        function shouldDisplayNextLesson($uv,$hour,$minutes) {
+            $start = explode(":", $uv['begin']);
+            return ((int)$start[0]) == $hour && ((int)$start[1]) == $minutes;
+        }
+
+
+        if (isset($_GET) && isset($_GET['login']) && !empty($_GET['login'])) { ?>
+            <!--<img src="loader.gif" alt="Loading..." /><br />-->
             <?php
             $json = file_get_contents("https://webapplis.utc.fr/Edt_ent_rest/myedt/result?login=".$_GET['login']);
             $data = json_decode($json, true);
@@ -23,41 +40,48 @@
             usort($data, function($a, $b) use ($days) {
                 return (array_search($a['day'], $days) - array_search($b['day'], $days)) * 2 + ((intval(str_replace(":","",$a['begin'])) - (intval(str_replace(":","",$b['begin']))) < 0) ? -1 : 1);
             });
-
-            //foreach ($data as $key => $value) {
-            //  foreach ($value as $k => $v) {
-            //    print $k.": ".$v."<br />";
-            //  }
-            //  print "<br />";
-            //}
             ?>
-            <!--<table border=1>
-                <tr>
-                  <td></td>
-                  <td>Lundi</td>
-                  <td>Mardi</td>
-                  <td>Mercredi</td>
-                  <td>Jeudi</td>
-                  <td>Vendredi</td>
-                  <td>Samedi</td>
-                  <td>Dimanche</td>
-                </tr>-->
-                <?php
-                //for ($day = 0; $day < count($days); $day++) {
-                //  $hour = 8; // < 20
-                //  $minutes = 0; // 0, 15, 30, 45, 0...
-                //  // array_shift($data) as we find a matching value
-                //  do {
-                //    print ""
-                //    if ($minutes = 45)
-                //      $hour++;
-                //    $minutes = ($minutes + 15) % 60;
-                //  } while($hour < 20)
-                //  <
-                //}
+            <table>
+            <?php
+            for ($day = 0; $day < count($days); $day++) {
+                print "<tr>";
+                print "<td>".$days[$day]."</td>";
+                $hour = 8; // < 20
+                $minutes = 0; // 0, 15, 30, 45, 0...
+                do {
+                    if (count($data) > 0 && shouldDisplayNextLesson($data[0],$hour,$minutes)) {
+                        $uv = array_shift($data);
 
-                ?>
-              <!--</table>-->
+                        $offset = getOffset($uv);
+                        $end = explode(":", $uv['end']);
+                        $hour = (int) $end[0];
+                        $minutes = (int) $end[1];
+                        if (isset($uvsColor[$uv['uv']])) {
+                            $color = $uvsColor[$uv['uv']];
+                        }
+                        else {
+                            $color = array_shift($colors);
+                            $uvsColor[$uv['uv']] = $color;
+                        }
+                        print "<td class='lesson' colspan='".$offset."' style='background-color: ".$color."'>";
+                        print "<span class='when'>".$uv['begin']." - ".$uv['end']."</span><br />";
+                        print $uv['type']. " " .$uv['uv'];
+                        if (!empty($uv['group']))
+                            print " (".$uv['group'].")";
+                        print "<br />".$uv['room'];
+                    }
+                    else {
+                        print "<td>";
+                        if ($minutes == 45)
+                            $hour++;
+                    $minutes = ($minutes + 15) % 60;
+                    }
+                    print "</td>";
+                } while($hour < 20);
+                print "</tr>";
+            }
+            ?>
+            </table>
             <?php
           }
           else { ?>
